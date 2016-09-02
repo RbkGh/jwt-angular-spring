440dd81c7d890e3723ea3004f8649ffac77e3cac
Ace P Rbk <rbk.unlimited@gmail.com>
2016-09-02 09:34:08 +0000

    Add new models,translate a model instead of throwing exception with raw string

---
+++ src/main/java/com/nibado/example/jwtangspr/model/ApiRequest.java
@@ -0,0 +1,9 @@
+package com.nibado.example.jwtangspr.model;
+
+/**
+ * @author Ace Programmer Rbk
+ *         <Rodney Kwabena Boachie at rbk.unlimited@gmail.com> on
+ *         02-Sep-16
+ */
+public class ApiRequest {
+}
